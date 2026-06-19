872b7e3b0f8e88ccd78bc52fc6c91fee9f6fc614
sagconsultantservice-design <sagconsultantservice@gmail.com>
2026-06-19 08:57:57 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,7 +14,7 @@ const values = [
 ];
 
 const founders = [
-  { name: "Akhil - CA CMA Semi qualified ", designation: "Founder ", color: "bg-accent" },
+  { name: "Akhil - CA ,CMA Semi qualified ", designation: "Founder ", color: "bg-accent" },
   { name: "Subha - CA ,CMA semi qualified", designation: "Co-Founder ", color: "bg-primary" },
   { name: "Gayathri - CA ,CMA  Semi qualified", designation: "Co-Founder", color: "bg-accent" },
 ];
